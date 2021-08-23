75f4184fcbc070bd70e928e324d8862ae48e72b1
elmarceloc <73359763+elmarceloc@users.noreply.github.com>
2021-08-22 23:52:22 -0400

    added function source
    this is useful for improve maintainability.

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,4 +1,5 @@
 /* eslint-disable-next-line import/prefer-default-export */
+// from: https://stackoverflow.com/questions/3115150/how-to-escape-regular-expression-special-characters-using-javascript
 export function escapeRegExp(text) {
   return text.replace(/[-[\]{}()+?.,\\^$|#\s]/g, '\\$&');
 }
